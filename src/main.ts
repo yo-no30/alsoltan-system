a5dbb212457bd30a297c7fd413e5756c8d3451cf
يوسف الصبري <yo-no30>
2026-09-08 01:57:15 +0300

    first commit

--- src/main.ts
+++ src/main.ts
@@ -8,10 +8,10 @@ import './assets/main.css'
 
 function escapeHtml(value: string): string {
   return value
-    .replaceAll('&', '&amp;')
-    .replaceAll('<', '&lt;')
-    .replaceAll('>', '&gt;')
-    .replaceAll('"', '&quot;')
+    .replace(/&/g, '&amp;')
+    .replace(/</g, '&lt;')
+    .replace(/>/g, '&gt;')
+    .replace(/"/g, '&quot;')
 }
 
 function renderStartupError(message: string): void {
